c3f6784589c3aa26d0ab49c14706cfd6ca3b9388
sayyidan.i <sayyidan2020@gmail.com>
2024-03-10 01:34:57 +0700

    initial commit, streaming data from API using kafka, airflow, and docker

---
+++ dag/kafka-stream.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 3, 9, 14, 00)
+}
+
+def get_data():
+    import requests
+    
+    res = requests.get('https://randomuser.me/api/')
+    res = res.json()
+    res = res['results'][0]
+    return res
+    
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, {location['city']}, {location['state']}, {location['country']}"
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['dob'] = res['dob']['date']
+    data['registered'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+    
+    return data
+    
+    
+
+def stream_data():
+    # Code to stream data from API
+    
+    from kafka import KafkaProducer
+    import time
+    import json
+    
+    
+    res = get_data()
+    res = format_data(res)
+    #print(json.dumps(res, indent=3))
+    
+    producer = KafkaProducer(bootstrap_servers='localhost:9092', max_block_ms=5000)
+    producer.send('user_created', json.dumps(res).encode('utf-8'))
+
+
+stream_data()
+""" with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
+    
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data,
+        provide_context=True
+    ) """
